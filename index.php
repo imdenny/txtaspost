8904bba4a7dde8e05e741c4e151d96489f017568
wppd <luan.bu@gmail.com>
2013-03-12 09:48:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 /*
  * Plugin Name:   TXTAsPost
  * Version:       3.0
- * Plugin URI:    http://www.wordpressplugindeveloper.com
+ * Plugin URI:    http://www.wpactions.com
  * Description:	  Convert TXT into wordpress posts
  * Author:        Jesse
- * Author URI:    http://www.wordpressplugindeveloper.com
+ * Author URI:    http://www.wpactions.com
  */
 require_once(dirname(__FILE__).'/txtaspost.php');
 register_activation_hook(__FILE__, array(&$TXTAsPost, 'install'));
